5f2a68c31e66098f6e555d5aaf7e41791ec5ad86
lucasfogaca <f.lucasaguiar@hotmail.com>
2026-02-13 21:40:04 -0300

    Update: Adicionando spring e jdk 21 para ser usado no backend

---
+++ src/main/java/com/example/heromode/HeromodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.heromode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeromodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeromodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heromode/HeromodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heromode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeromodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
