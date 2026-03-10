c797e46ad7af41476361e2d4b7f66640ad789997
lucasfogaca <f.lucasaguiar@hotmail.com>
2026-03-09 21:25:08 -0300

    Fix: Fixxing PlayerRepository and JwtService, with that Token and register worked, validated in Insomnia

--- src/main/java/com/example/heromode/features/authentication/JwtService.java
+++ src/main/java/com/example/heromode/features/authentication/JwtService.java
@@ -11,10 +11,10 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    @Value("$jwt.secret}")
+    @Value("${jwt.secret}")
     private String secret;
 
-    @Value("$jwt.expiration}")
+    @Value("${jwt.expiration}")
     private long expiration;
 
     private SecretKey getKey(){
--- src/main/java/com/example/heromode/features/progression/PlayerRepository.java
+++ src/main/java/com/example/heromode/features/progression/PlayerRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    Optional<Player> findByUsername(Long userId);
+    Optional<Player> findByUserId(Long userId);
 }
