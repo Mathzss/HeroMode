5568310d197a14058bbe51a23f185ddf90f4a0d1
lucasfogaca <f.lucasaguiar@hotmail.com>
2026-03-10 21:22:14 -0300

    Update: Creating authApi.js and updating api.js, preparing they for the new login screen

--- lifexp/src/services/api.js
+++ lifexp/src/services/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: 'http://localhost:8080/api' // Onde o seu Spring Boot está ouvindo
 });
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default api;
---
+++ lifexp/src/services/authApi.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const authApí = axio.create({
+    baseURL: "http://localhost:8080";
+});
+
+export default authApi;
