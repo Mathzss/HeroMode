d1b3487287e030270d71cee13484ae53bd3fb332
lucasfogaca <f.lucasaguiar@hotmail.com>
2026-04-01 21:27:43 -0300

    Update: update no cors

--- src/main/java/com/example/heromode/shared/CorsConfig.java
+++ src/main/java/com/example/heromode/shared/CorsConfig.java
@@ -15,7 +15,12 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://hero-mode.vercel.app",
+                "https://heromode.com.br",
+                "https://www.heromode.com.br"
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
